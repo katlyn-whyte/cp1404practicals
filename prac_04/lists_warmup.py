ff788c5e044167d78bbbb92d747502257a7558e4
katlyn <katlyn.whyte@my.jcu.edu.au>
2022-10-12 14:03:30 +1000

    Completed list warmups

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,24 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0] = 3
+# numbers[-1] = 2
+# numbers[3] = 1
+# numbers[:-1] = [3, 1, 4, 1, 5, 9]
+# numbers[3:4] = [1]
+# 5 in numbers = True
+# 7 in numbers = False
+# "3" in numbers = False
+# numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+
+# 1. change the first element to "ten"
+numbers[0] = "ten"
+# 2. change the last element to 1
+numbers[-1] = 1
+# 3. print all the elements from number except
+numbers[2:]
+# 4. print whether 9 is an element of numbers
+9 in numbers
+
+
+
