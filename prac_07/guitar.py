d15b51008b5708b8edaefad04abf1b0d01a7c15c
katlyn <katlyn.whyte@my.jcu.edu.au>
2022-11-09 19:13:02 +1000

    Completed first section of more guitars exercise

---
+++ prac_07/guitar.py
@@ -0,0 +1,21 @@
+CURRENT_YEAR = 2022
+VINTAGE_AGE = 50
+
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return f"{self.name} ({self.year}) : {self.cost:.2f}"
+
+    def get_age(self):
+        age = CURRENT_YEAR - self.year
+        return f"in {CURRENT_YEAR} the {self.name} is {age} years old"
+
+    def is_vintage(self):
+        return self.get_age() >= VINTAGE_AGE
+
+    def __lt__(self, other):
